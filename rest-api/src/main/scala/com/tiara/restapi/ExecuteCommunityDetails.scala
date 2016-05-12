731dcf766c9eaa4a1c46d9814b0ccd7e79f6eb94
Barbara Gomes <barbaramaltagomes@gmail.com>
2016-05-12 11:08:15 -0700

    [RDRK-205] Implement extratc sentiment by community for the communities details

--- rest-api/src/main/scala/com/tiara/restapi/ExecuteCommunityDetails.scala
+++ rest-api/src/main/scala/com/tiara/restapi/ExecuteCommunityDetails.scala
@@ -1,11 +1,12 @@
 package com.tiara.restapi
 
 import org.apache.spark.Logging
-import org.apache.spark.sql.Row
+import org.apache.spark.sql.{DataFrame, Row}
 import play.api.libs.json._
 import Utils._
 import redis.clients.jedis.Jedis
 import org.apache.spark.sql.functions._
+import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
 
 import scala.concurrent._
@@ -17,6 +18,8 @@ object ExecuteCommunityDetails extends Logging{
 
   val wordcloud = "wordcloud"
   val sentiment = "sentiment"
+  val sentimentDescription = Json.obj(sentiment -> Json.obj("communityID" -> Json.arr("positive", "negative", "neutral")))
+  val worldcloud = Json.obj(wordcloud -> Json.obj("communityID" -> Json.arr("word", "count")))
 
   def getDetails(searchTerms: String): Future[String] = {
 
@@ -26,40 +29,110 @@ object ExecuteCommunityDetails extends Logging{
     val result = future {computeDetails(searchTerms.toLowerCase(),md5)}
 
     result.recover{
-      case e: Exception => logError("Could not execute request.", e); Json.stringify(buildResponse(false,null))
+      case e: Exception => logError("Could not execute request.", e); Json.stringify(buildResponse(false,JsNull,JsNull))
     }
   }
 
   private def computeDetails(searchTerms: String, md5: String):String = {
+    try{
+      detailsFromJoin(md5, searchTerms)
+    }catch {
+      case e: Exception => logError("Could not get Details for community", e); Json.stringify(buildResponse(false,JsNull,JsNull))
+    }
+  }
+
+  private def detailsFromJoin(md5: String, searchTerms: String): String = {
+    //get Resource
     val jedis = ApplicationContext.jedisPool.getResource
+
+    logInfo("Loading membership from redis")
+    // Get communities from redis
     val communities = jedis.smembers(md5)
-    if(communities != null && !communities.isEmpty){
-      val it = communities.iterator()
-      var response = Json.obj()
-      while(it.hasNext){
-        val community = it.next()
-        response = response ++ getDetailsForCommunity(md5,community,jedis)
+    if(communities != null && !communities.isEmpty) {
+      val membership: ArrayBuffer[(String, Array[String])] = ArrayBuffer.empty
+      val it_com = communities.iterator()
+
+      // Populating membership array
+      while(it_com.hasNext){
+        val communityID = it_com.next()
+        val users = getUsersForCommunity(md5, communityID, jedis)
+        populateMembership(membership, communityID, users)
       }
-      jedis.close()
-      Json.stringify(buildResponse(true,response))
+
+      import ApplicationContext.sqlContext.implicits._
+      //Get membership as DF
+      val membershipDF = ApplicationContext.sparkContext
+                          .parallelize(membership).toDF
+                          .withColumnRenamed("_1", "community")
+                          .withColumnRenamed("_2", "uids")
+                          .select(col("community"), explode(col("uids")).as("uid"))
+
+      val filteredRT = getFilteredRT(searchTerms)
+      logInfo("Joining membership DF to Retweets DF")
+      val membershipRT_DF = filteredRT.join(membershipDF, filteredRT("uid") === membershipDF("uid")).cache()
+
+      // Get sentiment
+      val sentimentResponse:JsValue = extractSentiment(membershipRT_DF)
+
+      Json.stringify(buildResponse(true,sentimentResponse,JsNull))
     }else{
-      logInfo("Cache for graph expired or is not complete yet")
-      jedis.close()
-      Json.stringify(buildResponse(true,Json.obj()))
+      logInfo(s"No cached data for search: $searchTerms == MD5 $md5")
+      Json.stringify(buildResponse(false,Json.obj(),Json.obj()))
     }
   }
 
-  private def getDetailsForCommunity(md5: String, communityID: String, jResource: Jedis): JsObject = {
-    val userList:Array[String] = jResource.lrange(s"$md5:$communityID", 0, -1).toArray.map(_.toString)
-    if(userList != null && !userList.isEmpty){
-      val tweePerUserDF = InMemoryData.retweetsENDF.filter(col("uid").isin(userList:_*))
-      Json.obj(communityID -> Json.obj(wordcloud -> Json.arr(tweePerUserDF.count()), sentiment -> Json.arr(tweePerUserDF.count())))
-    }else{
-      Json.obj(communityID -> Json.obj(wordcloud -> Json.arr(), sentiment -> Json.arr()))
+  private def extractSentiment(membershipDF: DataFrame): JsValue = {
+
+    try {
+      val startTime = System.nanoTime()
+
+      logInfo("Extracting sentiment")
+      val sentiment = membershipDF.groupBy("community", "sentiment")
+        .agg(count("sentiment").as("count"))
+        .map(row => (row.getString(0), (row.getInt(1), row.getLong(2).toInt)))
+        .groupByKey
+
+      // Json.ojb(CommunityID -> Json.Array[count of positive, count of negative, count of neutral])
+      val mapToJson = sentiment.map(community_sentiment => {
+        Json.obj(community_sentiment._1 ->
+          Json.arr(community_sentiment._2.find { case (sent: Int, count: Int) => (sent, count) ==(1, count) }.getOrElse((0, 0))._2,
+            community_sentiment._2.find { case (sent: Int, count: Int) => (sent, count) ==(-1, count) }.getOrElse((0, 0))._2,
+            community_sentiment._2.find { case (sent: Int, count: Int) => (sent, count) ==(0, count) }.getOrElse((0, 0))._2))
+      }).collect
+
+      var jsonResponse = Json.obj()
+      mapToJson.foreach(communityData => jsonResponse = jsonResponse ++ communityData)
+
+      val elapsed = (System.nanoTime() - startTime) / 1e9
+      logInfo(s"Extract sentiment finished. Execution time: $elapsed")
+
+      jsonResponse
     }
+    catch {
+      case e: Exception => logError("Could not extract sentiment", e); JsNull
+    }
+
   }
 
-  private def buildResponse(success: Boolean, response: JsObject):JsObject = {
-    Json.obj("success" -> success) ++ Json.obj("communitydetails" -> response)
+  private def getFilteredRT(searchTerms: String): DataFrame = {
+    logInfo("Filtering Retweets DF")
+    val filterRegex = s"(${searchTerms.trim.replaceAll(","," | ")} )"
+    InMemoryData.retweetsENDF.filter(col(COL_TOKENS).rlike(filterRegex))
+  }
+
+  private def getUsersForCommunity(md5: String, communityID: String, jResource: Jedis): Array[String] = {
+    jResource.lrange(s"$md5:$communityID", 0, -1).toArray.map(_.toString)
+  }
+
+  private def populateMembership(membership:ArrayBuffer[(String, Array[String])], communityID: String, users: Array[String]) = {
+    if(users != null && !users.isEmpty)
+      membership.append((communityID,users))
+  }
+
+  private def buildResponse(success: Boolean, sentimentJson: JsValue, wordcloudJson: JsValue):JsObject = {
+    Json.obj("success" -> success) ++
+      sentimentDescription ++ worldcloud ++
+      Json.obj("communitydetails" -> (Json.obj(sentiment -> sentimentJson)
+        ++ Json.obj(wordcloud -> wordcloudJson)))
   }
 }
